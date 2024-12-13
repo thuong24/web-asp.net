07a7f6a7dcae03aa53909231f02610312500b861
Phan ngoc thuong <nt.6924.thuong@gmail.com>
2024-12-13 19:48:36 +0700

    start asp.net

---
+++ PhanNgocThuong_Web-ASP-NET/Web-ASP-NET/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_ASP_NET.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
